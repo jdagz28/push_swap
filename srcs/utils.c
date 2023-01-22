b63c9322ebd3aa272d0b6004d7e9d3cad7a5b49a
Joshua Dagoy <joshdagoy28@gmail.com>
2023-01-22 23:53:51 +0100

    22 Jan 2023

--- srcs/utils.c
+++ srcs/utils.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jdagoy <jdagoy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/21 14:21:06 by jdagoy            #+#    #+#             */
-/*   Updated: 2023/01/20 22:26:18 by jdagoy           ###   ########.fr       */
+/*   Created: 2022/12/06 14:21:06 by jdagoy            #+#    #+#             */
+/*   Updated: 2023/01/22 23:49:04 by jdagoy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,3 +23,16 @@ void    ft_swap(t_stack *stack)
     stack->next->value = tmp;
 }
 
+void    ft_push(t_stack **dest_stack, t_stack **src_stack)
+{
+    t_stack *tmp;
+
+    if ((*src_stack))
+        return ;
+    else
+    {
+        tmp = (*src_stack)->next;
+        ft_lstadd_front(dest_stack, *src_stack);
+        (*src_stack) = tmp;
+    }
+}
