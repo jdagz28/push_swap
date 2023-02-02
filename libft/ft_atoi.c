934297dc436f3ab81ff9b53bab4e03fc54714da7
Joshua Dagoy <jdagoy@shi-r10-p3.s19.be>
2023-02-02 14:20:12 +0100

    from fu

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: jdagoy <jdagoy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 14:21:47 by jdagoy            #+#    #+#             */
-/*   Updated: 2023/02/01 12:16:34 by jdagoy           ###   ########.fr       */
+/*   Updated: 2023/02/02 12:55:04 by jdagoy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,19 @@ int	ft_atoi(const char *str)
 	atoi = 0;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
 		i++;
-	if (str[i] == '-' || str[i] == '+')
+	if (str[i] == '-' || str[i] == '+')	
+	{
 		if (str[i++] == '-')
 			sign *= -1;
-	while (ft_isdigit(str[i]))
-		atoi = atoi * 10 + (str[i++] - '0');
+		if (!ft_isdigit(str[i]))
+			ft_error(NULL, "Error\n");
+	}
+	while (str[i])
+	{	
+		if (!ft_isdigit(str[i]))
+			ft_error(NULL, "Error\n");
+		atoi = atoi * 10 + (str[i] - '0');
+		i++;
+	}
 	return (ft_atoi_check(atoi * sign));
 }
